c99551dca3080a939ba6dfbbf037ca66c61ada07
loki <loki@uixlabs.in>
2025-02-22 20:23:33 +0530

    bio setup changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import type React from 'react'
 import './globals.css'
 
 const geistSans = Geist({
@@ -24,7 +25,11 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				{children}
+			</body>
 		</html>
 	)
 }
