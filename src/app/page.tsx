8c9e69b8e4cc31d501d9f6c34eeef4aba7564f7c
loki <loki@uixlabs.in>
2025-02-22 21:58:40 +0530

    Added nextjs font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className='grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20'>
+    <div className=''>
       <button
         type='button'
         className='rounded-md bg-purple-primary px-4 py-2 text-white'
