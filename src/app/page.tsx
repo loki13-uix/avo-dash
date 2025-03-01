2f5d2cef5728b13731e9c87046751ae528e05c61
Shaurya <shourya@uixlabs.in>
2025-03-01 12:04:55 +0530

    temp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,66 @@
 'use client'
 
-import Tree from '@/shared/components/atoms/tree'
-
+import { DataTable } from '@/shared/components/atoms/data-table'
+import { Checkbox } from '@/shared/components/ui/checkbox'
+import { ColumnDef } from '@tanstack/react-table'
 export default function Home() {
+  const columns: ColumnDef<{
+    id: string
+    name: string
+    age: number
+    email: string
+    phone: string
+    address: string
+  }>[] = [
+    {
+      header: () => {
+        return (
+          <div className='flex items-center justify-center'>
+            <Checkbox />
+          </div>
+        )
+      },
+      accessorKey: 'id',
+      cell: () => {
+        return (
+          <div className='flex items-center justify-center'>
+            <Checkbox />
+          </div>
+        )
+      },
+    },
+    {
+      header: 'Name',
+      accessorKey: 'name',
+    },
+    {
+      header: 'Age',
+      accessorKey: 'age',
+    },
+    {
+      header: 'Email',
+      accessorKey: 'email',
+    },
+    {
+      header: 'Phone',
+      accessorKey: 'phone',
+    },
+  ]
   return (
     <div className=''>
-      <Tree />
+      <DataTable
+        columns={columns}
+        data={[
+          {
+            name: 'John Doe',
+            age: 25,
+            email: 'john.doe@example.com',
+            phone: '1234567890',
+            address: '123 Main St, Anytown, USA',
+            id: ''
+          },
+        ]}
+      />
     </div>
   )
 }
---
+++ src/shared/components/atoms/data-table/index.tsx
@@ -0,0 +1,84 @@
+'use client'
+
+import {
+  type ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/shared/components/ui/table'
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
+  return (
+    <div className='rounded-md'>
+      <Table className='border-collapse border border-grey-3'>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id} className='border-b'>
+              {headerGroup.headers.map((header) => (
+                <TableHead
+                  key={header.id}
+                  className='border-r last:border-r-0 px-4 py-2'
+                >
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </TableHead>
+              ))}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow key={row.id} className='border-b'>
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell
+                    key={cell.id}
+                    className='border-r last:border-r-0 px-4 py-2'
+                  >
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell
+                colSpan={columns.length}
+                className='h-24 text-center border-r last:border-r-0'
+              >
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  )
+}
