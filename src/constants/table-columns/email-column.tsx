ff9b8083f61a18b214ed333b912ae445fb6f9cde
tanushiuix <tanushi@uixlabs.in>
2025-03-04 13:45:43 +0530

    added prop in email column

--- src/constants/table-columns/email-column.tsx
+++ src/constants/table-columns/email-column.tsx
@@ -9,18 +9,21 @@ type TableRow = {
 type EmailColumnProps = {
   row: TableRow
   isSelected: boolean
+  isReadOnly?: boolean
   onValueChange?: (id: string, value: string) => void
 }
 
 export const EmailColumn = ({
   row,
   isSelected,
+  isReadOnly,
   onValueChange,
 }: EmailColumnProps) => (
   <TableCell
     defaultValue={row.email}
     className='border-none w-full px-2 py-1.5'
     isSelected={isSelected}
+    isReadOnly={isReadOnly}
     onChange={(newValue) => {
       if (onValueChange) {
         onValueChange(row.id, newValue)
