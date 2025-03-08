99789bf37a3f9307de3a4979bda7564b60d758f6
Shaurya <shourya@uixlabs.in>
2025-03-08 21:02:41 +0530

    added screen with resizable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from '@/shared/components/ui/resizable'
+import useFileStore from '@/shared/store/store'
 
 import { Fragment, useEffect, useRef, useState } from 'react'
 
@@ -18,7 +19,6 @@ export default function Home() {
   const trees = [folderTree, folderTree]
   const panelRefs = useRef<(HTMLDivElement | null)[]>([null, null, null])
 
-  const [showTableComponent, setShowTableComponent] = useState(true)
   const [expandedIndices, setExpandedIndices] = useState<number[]>([0])
 
   const handleExpand = (index: number) => {
@@ -50,92 +50,101 @@ export default function Home() {
     panelRefs.current[index] = el
   }
 
-  function handleClick() {
-    setShowTableComponent((prev) => !prev)
-  }
+  const selectedFile = useFileStore((state) => state.selectedFile)
 
   return (
-    <>
+    <div className='h-screen overflow-hidden'>
       <div className='border-b border-grey-4 px-4 py-3'>
-        <Icon name='logo' size={24} />
+        <Icon name='logo' size={24} className='sticky top-0' />
       </div>
 
-      {/* Main layout with horizontal resizing */}
       <ResizablePanelGroup direction='horizontal' className='h-screen'>
-        {/* Sidebar Panel */}
         <ResizablePanel
           minSize={25}
           maxSize={40}
           defaultSize={25}
-          className='!overflow-auto'
+          className='!overflow-auto  z-1'
+          style={{ boxShadow: '0px 15px 40px 0px #CACAD58C' }}
         >
-          <div className='shadow-xl min-w-[340px] select-none flex h-screen flex-col overflow-auto'>
+          <div className='shadow-xl select-none flex h-screen flex-col overflow-auto'>
             <ResizablePanelGroup
               direction='vertical'
-              className='h-full !overflow-auto'
+              className='h-full !overflow-auto  max-h-[calc(100vh-50px)]'
             >
               <TableCell
                 isHeader
                 defaultValue='Project: '
-                className='px-3 py-4 border-none'
+                className='px-3 py-4 border-none !min-h-[54px]'
                 title='Web Project'
                 showIcon
-                iconOnClick={handleClick}
               />
-              {showTableComponent &&
-                trees.map((tree, index) => (
-                  <Fragment key={index}>
-                    <ResizablePanel
-                      minSize={3}
-                      className='!overflow-visible'
-                      defaultSize={expandedIndices.includes(index) ? 53 : 3}
-                    >
-                      <div ref={setPanelRef(index)}>
-                        <TreeWrapper
-                          key={index}
-                          initialTreeNodes={tree}
-                          isExpanded={expandedIndices.includes(index)}
-                          onExpand={() => handleExpand(index)}
-                          onCollapse={() => handleCollapse(index)}
-                        />
-                      </div>
-                    </ResizablePanel>
-                    {index !== trees.length - 1 && (
-                      <ResizableHandle className='!h-0.5 bg-muted' />
-                    )}
-                  </Fragment>
-                ))}
+              {trees.map((tree, index) => (
+                <Fragment key={index}>
+                  <ResizablePanel
+                    minSize={3}
+                    className='!overflow-visible'
+                    defaultSize={expandedIndices.includes(index) ? 53 : 3}
+                  >
+                    <div ref={setPanelRef(index)}>
+                      <TreeWrapper
+                        key={index}
+                        initialTreeNodes={tree}
+                        isExpanded={expandedIndices.includes(index)}
+                        onExpand={() => handleExpand(index)}
+                        onCollapse={() => handleCollapse(index)}
+                      />
+                    </div>
+                  </ResizablePanel>
+                  {index !== trees.length - 1 && (
+                    <ResizableHandle className='!h-0.5 bg-muted' />
+                  )}
+                </Fragment>
+              ))}
             </ResizablePanelGroup>
           </div>
         </ResizablePanel>
 
-        {/* Horizontal Resize Handle */}
-        <ResizableHandle withHandle className='bg-gray-200' />
+        <ResizableHandle />
 
-        <ResizablePanel defaultSize={75} className='overflow-auto'>
-          <div className='flex h-full flex-col bg-grey-2'>
-            <div className='w-full flex justify-end border-t-0 p-4'>
-              <TableCellActions
-                name={['plus-icon', 'pen', 'file-data', 'trash', 'dots']}
-                iconClassName='size-5'
-                iconColors='#8793A0'
-                className='border-none gap-x-4'
-              />
-              <Button
-                variant='success'
-                className='border-none'
-                icon='save'
-                iconClassName='size-5'
-              >
-                Save
-              </Button>
-            </div>
-            <div className='px-4 flex-1 overflow-auto'>
-              <TableComponent className='bg-white' />
-            </div>
+        <ResizablePanel
+          defaultSize={70}
+          className='overflow-auto !w-[calc(100vw-500px)]'
+        >
+          <div className='flex h-full flex-col bg-grey-1'>
+            {selectedFile.length > 0 && (
+              <div className='w-full flex justify-end border-t-0 p-4 gap-1.5'>
+                <TableCellActions
+                  name={[
+                    'copy',
+                    'paste',
+                    'trash',
+                    'import',
+                    'export',
+                    'details',
+                  ]}
+                  iconClassName='size-5'
+                  iconColors='#8793A0'
+                  className='border-none gap-x-4'
+                />
+                <Button
+                  variant='success'
+                  className='border-none px-1.5 py-1 w-[78px] h-8'
+                  icon='save'
+                  iconClassName='size-5'
+                >
+                  Save
+                </Button>
+              </div>
+            )}
+
+            {selectedFile.length > 0 && (
+              <div className='px-4 flex-1 overflow-auto'>
+                <TableComponent className='bg-white' />
+              </div>
+            )}
           </div>
         </ResizablePanel>
       </ResizablePanelGroup>
-    </>
+    </div>
   )
 }
